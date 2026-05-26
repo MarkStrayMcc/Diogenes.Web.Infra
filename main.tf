3c5c5df8a47a18e89b00b7093719b397f61aa9d5
Mark McCormick <mark@diogenes.no>
2026-05-26 13:07:05 +0100

    Import ECS clusters ECR and security groups

--- main.tf
+++ main.tf
@@ -17,3 +17,47 @@ resource "aws_ecs_cluster" "uat" {
     }
   }
 }
+
+resource "aws_ecr_repository" "diogenes_web" {
+  name = "diogenes-web"
+}
+
+resource "aws_security_group" "alb" {
+  name        = "diogenes-alb-sg"
+  description = "Diogenes LB SG"
+  vpc_id      = "vpc-0f662ffab3ecc56a9"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "dev_service" {
+  name        = "diogenes-web-service-sg"
+  description = "Created in ECS Console"
+  vpc_id      = "vpc-0f662ffab3ecc56a9"
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
