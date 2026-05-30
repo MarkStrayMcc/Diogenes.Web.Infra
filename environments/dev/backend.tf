58f43e523e410a38733d1412247a3f9ba9826873
Mark McCormick <mark@diogenes.no>
2026-05-30 12:36:54 +0100

    Terraform formatting fixed

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "diogenes-terraform-state"
-    key            = "diogenes-web/dev/terraform.tfstate"
-    region         = "eu-west-2"
-    use_lockfile   = true
-    encrypt        = true
+    bucket       = "diogenes-terraform-state"
+    key          = "diogenes-web/dev/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+    encrypt      = true
   }
 }
